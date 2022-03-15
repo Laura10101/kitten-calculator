691c9fa38a141926aa8c3c7556b3946589b0961f
Laura Findley <laurjanefindley@gmail.com>
2022-03-15 14:58:32 +0000

    Add functionality to show and traverse questions and answers

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,21 +1,36 @@
-let sections = [document.getElementById('introduction'),
+//Get application data
+const questions = getQuestions();
+
+//Paths
+const imgs = 'assets/img/';
+const imgExt = '.png';
+
+//Get references to DOM elements for sections
+const sections = [document.getElementById('introduction'),
                 document.getElementById('questions'),
                 document.getElementById('results')];
+
+//Get references to DOM elements for questions 
+const questionText = document.getElementById('question-text');
+const answerContainer = document.getElementById('answer-container');
+const answerTemplate = document.getElementById('answer-template');
+const previousQuestionButton = document.getElementById('previous-question-button');
+const nextQuestionButton = document.getElementById('next-question-button');
 
 //Initialise calculator
 initialiseCalculator();
 
-// create function to pull questions in, to keep ui.js smaller and more focused: single reponsibility principle
+// Create function to pull questions in, to keep ui.js smaller and more focused: single reponsibility principle
 function initialiseCalculator() {
     currentQuestion = 0;
-    questions = getQuestions();
+    
     maternalAnswers = [];
     paternalAnswers = [];
     showIntroSection();
 }
 
-//these 3 functiuons control the display of the different sections 
-//used 3 functions to reduce the chance of errors caused by incrementing current section too many times 
+//These 3 functions control the display of the different sections 
+//Used 3 functions to reduce the chance of errors caused by incrementing current section too many times 
 
 function showIntroSection() {
     currentSection = 0;
@@ -42,4 +57,51 @@ function showCurrentSection() {
     sections[currentSection].classList.add("current-section")
 }
 
+//These functions traverse through the different questions 
+function showNextQuestion() {
+    currentQuestion = currentQuestion + 1;
+    showCurrentQuestion();
+}
 
+function showPreviousQuestion() {
+    currentQuestion = currentQuestion - 1;
+    showCurrentQuestion();
+}
+
+function showCurrentQuestion() {
+    //Get current question from questions object 
+    let question = questions[currentQuestion];
+    let answers = question.answers;
+    //Show question
+    questionText.innerText = question.text;
+    //Display question answers
+    for (let i = 0; i < answers.length; i++) {
+        //Duplicate answer template
+        let answerButton = answerTemplate.cloneNode(true);
+        let answerText = answerButton.getElementById('answer-description-template');
+        let answerImage = answerButton.getElementById('answer-image-template');
+
+        //Create an id for the answer
+        answerId = 'q' + currentQuestion + '-' + answers[i].text.replace('  ', ' ').replace(' ', '-');
+        //Set answer description 
+        answerText.innerText = answers[i].text; 
+        
+        //Set answer image
+        imagePath = imgs + answerId + imgExt;
+        answerImage.src = imagePath;
+        
+        //Set answer button ID
+        answerButton.id = answerId + '-answer-button';
+        
+        //Set the answer image ID
+        answerImage.id = answerId + '-answer-image';
+        
+        //Set the answer text ID
+        answerText.id = answerId + 'answer-description';
+        
+        //Add the new answer button to the DOM
+        answerContainer.childNodes.add(answerButton);
+    }
+    //Enable or disable the previous and next buttons, depending on which question we are on
+
+}
