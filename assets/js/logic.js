5d54feb803bea6e31dcaa005dd11f5a3d1b31861
Laura Findley <laurjanefindley@gmail.com>
2022-04-10 16:43:25 +0100

    Define logic functions to calculate kitten phenotypes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,17 @@
-function calculateKittens(answers) {
+function calculateKittens(maternalGenotype, paternalGenotype, genes) {
+    let possibleGenotypes = calculatePossibleGenotypes(maternalGenotype, paternalGenotype, genes, 0)
+    let possiblePhenotypes = determinePhenotypes(possibleGenotypes)
+    return calculateProbabilities(possiblePhenotypes)
+}
+
+function calculatePossibleGenotypes (maternalGenotype, paternalGenotype, genes, currentGene) {
+
+}
+
+function determinePhenotypes(possibleGenotypes) {
+
+}
+
+function calculateProbabilities(possiblePhenotypes) {
     
 }
