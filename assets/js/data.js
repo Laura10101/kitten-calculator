8efaa8fc57eb2aac6212c4b6ebb9a698b6866a29
Laura Findley <laurjanefindley@gmail.com>
2022-04-18 13:51:46 +0100

    Add gene mappings to each answer

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,81 +1,289 @@
 function getQuestions() {
     return [{
-        'text' : 'Please choose the base colour',
-        'answers' : [
-            { 'text' : 'Black', 'preconditions' : [] },
-            { 'text' : 'Blue', 'preconditions' : [] },
-            { 'text' : 'Chocolate', 'preconditions' : [] },
-            { 'text' : 'Lilac', 'preconditions' : [] },
-            { 'text' : 'Cinnamon', 'preconditions' : [] },
-            { 'text' : 'Fawn', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "B",
+                        "p2": "B"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": false,
+                        "p2": false
+                    }
+                ],
+                "preconditions": [],
+                "text": "Black"
+            },
+            {
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "B",
+                        "p2": "B"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": true,
+                        "p2": true
+                    }
+                ],
+                "preconditions": [],
+                "text": "Blue"
+            },
+            {
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "b",
+                        "p2": "b"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": false,
+                        "p2": false
+                    }
+                ],
+                "preconditions": [],
+                "text": "Chocolate"
+            },
+            {
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "b",
+                        "p2": "b"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": true,
+                        "p2": true
+                    }
+                ],
+                "preconditions": [],
+                "text": "Lilac"
+            },
+            {
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "b1",
+                        "p2": "b1"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": false,
+                        "p2": false
+                    }
+                ],
+                "preconditions": [],
+                "text": "Cinnamon"
+            },
+            {
+                "geneMapping": [{
+                        "gene": "colour",
+                        "p1": "b1",
+                        "p2": "b1"
+                    },
+                    {
+                        "gene": "dilute",
+                        "p1": true,
+                        "p2": true
+                    }
+                ],
+                "preconditions": [],
+                "text": "Fawn"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : []
+        "multiselect": false,
+        "preconditions": [],
+        "text": "Please choose the base colour"
     },
-
     {
-        'text' : 'Select any other genes carried by this parent',
-        'answers' : [
-            { 'text' : 'Dilute', 'preconditions' : [ { 'questionIndex' : 0, 'validAnswerIndices' : [ 0, 2, 4 ] } ] }, //Dilute can only be carried by black, chocolate, cinnamon
-            { 'text' : 'Chocolate', 'preconditions' : [ { 'questionIndex' : 0, 'validAnswerIndices' : [ 0, 1 ] } ] }, //Chocolate can only be carried by black or blue
-            { 'text' : 'Cinnamon', 'preconditions' : [ { 'questionIndex' : 0, 'validAnswerIndices' : [ 0, 1, 2, 3 ] } ] }, //Cinnamon can only be carried by black, blue, chocolate or lilac
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "dilute",
+                    "p2": true
+                }],
+                "preconditions": [{
+                    "questionIndex": 0,
+                    "validAnswerIndices": [
+                        0,
+                        2,
+                        4
+                    ]
+                }],
+                "text": "Dilute"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "colour",
+                    "p2": "b"
+                }],
+                "preconditions": [{
+                    "questionIndex": 0,
+                    "validAnswerIndices": [
+                        0,
+                        1
+                    ]
+                }],
+                "text": "Chocolate"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "colour",
+                    "p2": "b1"
+                }],
+                "preconditions": [{
+                    "questionIndex": 0,
+                    "validAnswerIndices": [
+                        0,
+                        1,
+                        2,
+                        3
+                    ]
+                }],
+                "text": "Cinnamon"
+            }
         ],
-        'multiselect' : true,
-        'preconditions' : []
+        "multiselect": true,
+        "preconditions": [],
+        "text": "Select any other genes carried by this parent"
     },
-
     {
-        'text' : 'This parent is',
-        'answers' : [
-            { 'text' : 'Tabby', 'preconditions' : [] },
-            { 'text' : 'Non-tabby', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "tabby",
+                    "p1": true,
+                    "p2": true
+                }],
+                "preconditions": [],
+                "text": "Tabby"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "tabby",
+                    "p1": false,
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "Non-tabby"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : []
+        "multiselect": false,
+        "preconditions": [],
+        "text": "This parent is"
     },
-
     {
-        'text' : 'Does this parent carry self?',
-        'answers' : [
-            { 'text' : 'Yes', 'preconditions' : [] },
-            { 'text' : 'No', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "tabby",
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "Yes"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "tabby",
+                    "p2": true
+                }],
+                "preconditions": [],
+                "text": "No"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : [ { 'questionIndex' : 2, 'validAnswerIndices' : [ 0 ] } ]
+        "multiselect": false,
+        "preconditions": [{
+            "questionIndex": 2,
+            "validAnswerIndices": [
+                0
+            ]
+        }],
+        "text": "Does this parent carry self?"
     },
-
     {
-        'text' : 'This parent is',
-        'answers' : [
-            { 'text' : 'Self', 'preconditions' : [] },
-            { 'text' : 'Bicolour', 'preconditions' : [] },
-            { 'text' : 'Van', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "whiteSpotting",
+                    "p1": false,
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "Self"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "whiteSpotting",
+                    "p1": true,
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "Bicolour"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "whiteSpotting",
+                    "p1": true,
+                    "p2": true
+                }],
+                "preconditions": [],
+                "text": "Van"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : []
+        "multiselect": false,
+        "preconditions": [],
+        "text": "This parent is"
     },
-
     {
-        'text' : 'This parent is',
-        'answers' : [
-            { 'text' : 'Non-pointed', 'preconditions' : [] },
-            { 'text' : 'Colourpoint', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "colourpoint",
+                    "p1": false,
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "Non-pointed"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "colourpoint",
+                    "p1": true,
+                    "p2": true
+                }],
+                "preconditions": [],
+                "text": "Colourpoint"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : []
+        "multiselect": false,
+        "preconditions": [],
+        "text": "This parent is"
     },
-
     {
-        'text' : 'Does this parent carry colourpoint?',
-        'answers' : [
-            { 'text' : 'Yes', 'preconditions' : [] },
-            { 'text' : 'No', 'preconditions' : [] },
+        "answers": [{
+                "geneMapping": [{
+                    "gene": "colourpoint",
+                    "p2": true
+                }],
+                "preconditions": [],
+                "text": "Yes"
+            },
+            {
+                "geneMapping": [{
+                    "gene": "colourpoint",
+                    "p2": false
+                }],
+                "preconditions": [],
+                "text": "No"
+            }
         ],
-        'multiselect' : false,
-        'preconditions' : [ { 'questionIndex' : 5, 'validAnswerIndices' : [ 0 ] } ]
-    }]
+        "multiselect": false,
+        "preconditions": [{
+            "questionIndex": 5,
+            "validAnswerIndices": [
+                0
+            ]
+        }],
+        "text": "Does this parent carry colourpoint?"
+    }
+];
 }
 
 function getGenes() {
-    return ['colour', 'dilute', 'tabby', 'colourpoint', 'whiteSpotting']
+    return ["colour", "dilute", "tabby", "colourpoint", "whiteSpotting"];
 }
