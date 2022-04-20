07a835f1695f0548d4e8e5a97bb79c58e6da64ba
Laura Findley <laurjanefindley@gmail.com>
2022-04-20 13:33:17 +0100

    Add automated tests for colourpoint gene

--- assets/js/tests/logic.test.js
+++ assets/js/tests/logic.test.js
@@ -1,7 +1,21 @@
-const determineWhitespotting = require("../logic");
+const { determineWhitespotting, determineColourpoint } = require("../logic");
 
 describe("Determining phenotypes", () => {
     describe("Determine white spotting", () => {
+        test("true and true for white spotting returns van", () => {
+            expect(determineWhitespotting(true, true)).toEqual("van");
+        });
+        test("true and false for white spotting returns bicolour", () => {
+            expect(determineWhitespotting(true, false)).toEqual("bicolour");
+        });
+        test("false and true for white spotting returns bicolour", () => {
+            expect(determineWhitespotting(false, true)).toEqual("bicolour");
+        });
+        test("false and false for white spotting returns ''", () => {
+            expect(determineWhitespotting(false, false)).toEqual("");
+        });
+    });
+    describe("Determine colourpoint", () => {
         test("true and true for white spotting returns van", () => {
             expect(determineWhitespotting(true, true)).toEqual("van");
         });
