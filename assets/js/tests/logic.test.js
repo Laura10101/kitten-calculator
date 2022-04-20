762c7ad5f60901cab93ec8670f8d50467763b157
Laura Findley <laurjanefindley@gmail.com>
2022-04-20 14:53:20 +0100

    Add tests for determineTabby()

--- assets/js/tests/logic.test.js
+++ assets/js/tests/logic.test.js
@@ -16,17 +16,18 @@ describe("Determining phenotypes", () => {
         });
     });
     describe("Determine colourpoint", () => {
-        test("true and true for white spotting returns van", () => {
-            expect(determineWhitespotting(true, true)).toEqual("van");
+        test("true and true for colourpoint returns point", () => {
+            expect(determineColourpoint(true, true)).toEqual("point");
         });
-        test("true and false for white spotting returns bicolour", () => {
-            expect(determineWhitespotting(true, false)).toEqual("bicolour");
+        test("true and false for colourpoint returns ''", () => {
+            expect(determineColourpoint(true, false)).toEqual("");
         });
-        test("false and true for white spotting returns bicolour", () => {
-            expect(determineWhitespotting(false, true)).toEqual("bicolour");
+        test("false and true for colourpoint returns ''", () => {
+            expect(determineColourpoint(false, true)).toEqual("");
         });
-        test("false and false for white spotting returns ''", () => {
-            expect(determineWhitespotting(false, false)).toEqual("");
+        test("false and false for colourpoint returns ''", () => {
+            expect(determineColourpoint(false, false)).toEqual("");
         });
     });
+    
 });
